f58b76db1a38f810753d27128b5ac9aff03c1e04
Yuna <yunazhu1@gmail.com>
2020-09-18 17:09:46 +0800

    make it better

--- serve.js
+++ serve.js
@@ -1,8 +1,2 @@
 var http =require('http')
-http.createServer(function (request, response){
-
-response.writeHead(200,{"Content-Type":"text/plain"})
-response.end( "Hello world\n")
-I
-}).listen(3000)
-log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+console.log(http)
